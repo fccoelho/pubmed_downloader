108808e3361b3ade7209f26586df8c25b7e6904a
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-03-07 16:32:45 -0300

    Capture tested and working

--- fetch.py
+++ fetch.py
@@ -9,6 +9,7 @@ __docformat__ = 'restructuredtext en'
 from Bio import Entrez
 import pymongo
 
+
 connection = pymongo.MongoClient()
 
 
@@ -18,8 +19,29 @@ class SearchAndCapture:
         Entrez.email = email
         self.collection = connection.pubmed.articles
 
+    def _fetch(self, pmid):
+        handle = Entrez.efetch(db="pubmed", id=pmid, retmode='xml')
+        records = list(Entrez.parse(handle))
+        return records
+
+    def _get_old_ids(self):
+        oldids = self.collection.find({}, {"MedlineCitation.PMID": 1})
+        oldids = [i['MedlineCitation']['PMID'] for i in oldids]
+        return oldids
+
     def update(self):
-        handle = Entrez.esearch("pubmed", term=self.search_term)
+        old_ids = self._get_old_ids()
+        handle = Entrez.esearch(db="pubmed", retmax=1000, term=self.search_term)
         response = Entrez.read(handle=handle)
+
+        new_ids = {}
+        for pmid in response['IdList']:
+            if pmid in old_ids:
+                continue
+            art = self._fetch(pmid)[0]
+            result = self.collection.update_one({"MedlineCitation.PMID": art["MedlineCitation"]["PMID"]}, {"$setOnInsert": art}, upsert=True)
+            new_ids[pmid] = result.upserted_id
+        return new_ids
+
 
 
--- test_fetch.py
+++ test_fetch.py
@@ -2,11 +2,31 @@
 __author__ = 'fccoelho'
 
 import unittest
+from fetch import SearchAndCapture
+from bson.objectid import ObjectId
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self):
+        self.S = SearchAndCapture('fccoelho@gmail.com', 'zika virus')
+
+    def test_fetch_one_article(self):
+        resp = self.S._fetch('26923117')
+        self.assertIsInstance(resp, list)
+        self.assertEqual(len(resp), 1)
+
+    def test_get_old_ids(self):
+        res = self.S._get_old_ids()
+        self.assertIsInstance(res[0], str)
+
+
+    def test_update_database(self):
+        ids = self.S.update()
+        self.assertIsInstance(ids, dict)
+        self.assertGreater(len(ids), 0)
+        obj = list(ids.values())[0]
+        self.assertIsInstance(obj, ObjectId)
+
 
 
 if __name__ == '__main__':
