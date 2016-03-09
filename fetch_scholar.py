360d5244f0a5b3e62e21bbe56795dec0e9d15081
Flavio C. Coelho <fccoelho@gmail.com>
2016-03-08 21:39:08 -0300

    improved scholar search

--- fetch_scholar.py
+++ fetch_scholar.py
@@ -12,22 +12,32 @@ conn = pymongo.MongoClient()
 search_query = scholarly.search_pubs_query('zika zikv -author:zika')
 
 def continuous_fetch():
+    downloaded = [a['url_scholarbib'] for a in conn.scholar.articles.find({}, {'url_scholarbib': 1})]
     while True:
         doc = {}
         try:
             art = next(search_query)
+            if art.url_scholarbib in downloaded:
+                continue
             if not art._filled:
                 art.fill()
             doc['bib'] = art.bib
-            doc['citedby'] = art.citedby
-            doc['id_scholarcitedby'] = art.id_scholarcitedby
+            try:
+                doc['citedby'] = art.citedby
+            except AttributeError:
+                doc['citedby'] = 0
+            try:
+                doc['id_scholarcitedby'] = art.id_scholarcitedby
+            except AttributeError:
+                doc['id_scholarcitedby'] = ""
             doc['url_scholarbib'] = art.url_scholarbib
             doc['source'] = art.source
             conn.scholar.articles.update_one({'url_scholarbib': art.url_scholarbib}, {"$setOnInsert": doc}, upsert=True)
+            print("inserted {}".format(art.url_scholarbib))
         except Exception as e:
             print(e, art)
             break
-        time.sleep(0.5)
+        time.sleep(0.1)
 
 if __name__ == "__main__":
     continuous_fetch()
