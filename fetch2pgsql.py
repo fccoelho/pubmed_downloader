3390e6cd3267a65d0b35d1871ea50ba98c785f72
Flávio Codeço Coelho <fccoelho@gmail.com>
2021-11-09 10:33:48 -0300

    concurrent download for articles with rate limiting and using API KEY
    Also ported the analysis script to Postgresql based corpora

--- fetch2pgsql.py
+++ fetch2pgsql.py
@@ -13,27 +13,29 @@ from getpass import getpass
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import pandas as pd
 from datetime import date
+from ratelimiter import RateLimiter
 
 
 pw = getpass("Enter database password: ")
+KEY = open('ENTREZ_KEY').read()
 
-engine = create_engine(f"postgresql://postgres:{pw}@localhost/pubmed")
+engine = create_engine(f"postgresql://postgres:{pw}@localhost/pubmed",pool_size=20, max_overflow=0)
 
 
 zika_query_strings = ['((zika) NOT zika[Author])',
-                 '((zika virus NOT zika[Author]))',
-                 '((zika microcephaly) NOT zika[author])',
-                 '((zika fever NOT zika[Author]))',
-                 '(zika NOT zika[Author] ) guillain barre',
-                 '((zika epidemic NOT zika[Author]))',
-                 '((zika model NOT zika[Author]))',
-                 '(zika NOT zika[Author] ) chikungunya',
-                 '(zika NOT zika[Author] ) outbreak',
-                 '(zika NOT zika[Author] ) dengue',
-                 '(zika NOT zika[Author]) culex ',
-                 '(zika NOT zika[Author]) mosquito',
-                 '(zika NOT zika[Author])virus transmission'
-                 ]
+                      '((zika virus NOT zika[Author]))',
+                      '((zika microcephaly) NOT zika[author])',
+                      '((zika fever NOT zika[Author]))',
+                      '(zika NOT zika[Author] ) guillain barre',
+                      '((zika epidemic NOT zika[Author]))',
+                      '((zika model NOT zika[Author]))',
+                      '(zika NOT zika[Author] ) chikungunya',
+                      '(zika NOT zika[Author] ) outbreak',
+                      '(zika NOT zika[Author] ) dengue',
+                      '(zika NOT zika[Author]) culex ',
+                      '(zika NOT zika[Author]) mosquito',
+                      '(zika NOT zika[Author])virus transmission'
+                      ]
 MERS_query_strings = [
     'MERS',
     'mers cov',
@@ -49,18 +51,33 @@ Mayaro_query_strings = ['mayaro',
 Oropouche_query_strings = ['oropouche',
                            'oropouche virus'
                            ]
-months = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6,'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
+
+Covid_query_strings = ['covid19',
+                       'covid-19',
+                       'sars-cov-2',
+                       '2019-ncov',
+                       'novel coronavirus'
+                       ]
+months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6,
+          'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
+
+
 class SearchAndCapture:
     def __init__(self, email, search_term, collection='articles'):
-        self.search_term =search_term
+        self.search_term = search_term
         Entrez.email = email
-        self.articles_table = "articles"
-        self.citation_table = "citations"
+        Entrez.api_key = KEY
+        self.articles_table = collection
+        self.citation_table = 'citations' if collection == 'articles' else f"citations_{collection}"
         with engine.connect() as connection:
-            connection.execute("create table IF NOT EXISTS articles(pmid integer unique, title varchar(1024), abstract text, journal varchar(512), pubdate date);")
-            connection.execute("create table IF NOT EXISTS citations(pmid integer unique, cited_by text);")
-            connection.execute(f"create unique index IF NOT EXISTS pmid_idx on {self.articles_table} (pmid)")
-            connection.execute(f"create unique index IF NOT EXISTS pmid_idx on {self.citation_table} (pmid)")
+            connection.execute(
+                f"create table IF NOT EXISTS {self.articles_table}(pmid integer unique, title varchar(1024), abstract text, journal varchar(512), pubdate date);")
+            connection.execute(
+                f"create table IF NOT EXISTS {self.citation_table}(pmid integer unique, cited_by text);")
+            connection.execute(
+                f"create unique index IF NOT EXISTS pmid_idx on {self.articles_table} (pmid)")
+            connection.execute(
+                f"create unique index IF NOT EXISTS pmid_idx on {self.citation_table} (pmid)")
         # self.collection = connection.pubmed[collection]
         # self.collection.create_index([('$**', pymongo.TEXT)], default_language='english')
         # if collection == 'articles':
@@ -68,21 +85,31 @@ class SearchAndCapture:
         # else:
         #     self.citation_colection = connection.pubmed["citations_{}".format(collection)]
 
+    @RateLimiter(max_calls=5, period=1)
     def _fetch(self, pmid):
-        handle = Entrez.efetch(db="pubmed", id=pmid, retmode='xml')
-        records = Entrez.read(handle)
-        return records.get('PubmedArticle',[])
+        try:
+            handle = Entrez.efetch(db="pubmed", id=pmid, retmode='xml')
+            records = Entrez.read(handle)
+            return records.get('PubmedArticle', [])
+        except URLError as ue:
+            print(f"Downloading of {pmid} failed. {ue}\r")
+            return
+        except IndexError as e:
+            print("Empty record for {}\r".format(pmid))
+            return
 
     def _get_old_ids(self):
         with engine.connect() as connection:
-            res = connection.execute(f"select pmid from {self.articles_table};")
+            res = connection.execute(
+                f"select pmid from {self.articles_table};")
             oldids = res.fetchall()
-        
+
         return [i[0] for i in oldids]
 
     def _get_citations(self, pmid):
         try:
-            handle = Entrez.elink(dbfrom="pubmed", id=pmid, linkname="pubmed_pubmed_citedin")
+            handle = Entrez.elink(dbfrom="pubmed", id=pmid,
+                                  linkname="pubmed_pubmed_citedin")
         except HTTPError as e:
             print(e)
             return None
@@ -96,19 +123,20 @@ class SearchAndCapture:
 
     def create_FT_index(self):
         with engine.connect() as connection:
-            query = f"ALTER TABLE articles ADD COLUMN textsearchable_index_col tsvector\
+            query = f"ALTER TABLE {self.articles_table} ADD COLUMN textsearchable_index_col tsvector\
                GENERATED ALWAYS AS (to_tsvector('english', coalesce(title, '') || ' ' || coalesce(abstract, ''))) STORED;"
-            query2 = "CREATE INDEX textsearch_idx ON articles USING GIN (textsearchable_index_col);"
+            query2 = "CREATE INDEX textsearch_idx ON {self.articles_table} USING GIN (textsearchable_index_col);"
             connection.execute(query)
 
     def update_citations_concurrently(self):
-        print("Updating citations...")
+        print("Updating citations...\r")
         ids = self._get_old_ids()
         with ThreadPoolExecutor(max_workers=100) as executor:
-            future_cits = {executor.submit(self._get_citations, pmid): pmid for pmid in ids}
+            future_cits = {executor.submit(
+                self._get_citations, pmid): pmid for pmid in ids}
             for future in as_completed(future_cits):
                 pmid = future_cits[future]
-                print("getting citations for {}".format(pmid))
+                print("getting citations for {}\r".format(pmid))
                 try:
                     cits = future.result()
                     if cits is None:
@@ -117,7 +145,7 @@ class SearchAndCapture:
                         with engine.connect() as connection:
                             cits = '|'.join(cits)
                             connection.execute(f"insert into {self.citation_table} VALUES(%s, %s) \
-                            on conflict (pmid) do update set cited_by=%s;",(pmid, cits,cits))
+                            on conflict (pmid) do update set cited_by=%s;", (pmid, cits, cits))
                 except Exception as e:
                     print(f'{pmid} generated an exception: {e}')
 
@@ -129,11 +157,11 @@ class SearchAndCapture:
             if cits is None:
                 continue
             with engine.connect() as connection:
-                            cits = '|'.join(cits)
-                            connection.execute(f"insert into {self.citation_table} VALUES(%s, %s) \
-                            on conflict (pmid) do update set cited_by=%s;",(pmid, cits,cits))
+                cits = '|'.join(cits)
+                connection.execute(f"insert into {self.citation_table} VALUES(%s, %s) \
+                            on conflict (pmid) do update set cited_by=%s;", (pmid, cits, cits))
 
-    def update_multiple_searches(self, queries=None):
+    def update_multiple_searches(self, queries=None, parallel=False):
 
         if queries is not None:
             query_strings = queries
@@ -141,65 +169,100 @@ class SearchAndCapture:
             query_strings = [self.search_term]
         for qs in query_strings:
             self.search_term = qs
-            self.update()
+            if parallel:
+                self.update_concurrently()
+            else:
+                self.update()
 
     def update(self):
         old_ids = self._get_old_ids()
-        # print (old_ids)
-        # return
-        print("Fetching results for {}".format(self.search_term))
-        handle = Entrez.esearch(db="pubmed", retmax=100000, term=self.search_term)
+        print("Fetching results for {}\r".format(self.search_term))
+        handle = Entrez.esearch(
+            db="pubmed", retmax=100000, term=self.search_term)
         response = Entrez.read(handle=handle)
-        print("Found {} items".format(len(response['IdList'])))
+        print("Found {} items\r".format(len(response['IdList'])))
         new_ids = {}
         for pmid in tqdm(response['IdList']):
             if int(pmid) in old_ids:
                 continue
-            try:
-                art = self._fetch(pmid)[0]
-            except URLError:
-                print("Downloading of {} failed. Skipping".format(pmid))
+            art = self._fetch(pmid)
+            if art is None:
                 continue
-            except IndexError as e:
-                print("Empty record for {}".format(pmid))
-                continue
+            else:
+                art = art[0]
+
             if "MedlineCitation" not in art:
                 continue
-            # TODO: Fix the date before insertion
-            with engine.connect() as connection:
-                article = art["MedlineCitation"]["Article"]
-                try:
-                    abstract = article['Abstract']['AbstractText'][0].replace("'","''")
-                except (AttributeError, KeyError) as e:
-                    print(e)
-                    abstract = ""
-                title = article["ArticleTitle"].replace("'","''")
-                journal = article["Journal"]["Title"]
-                year = int(article["Journal"]["JournalIssue"]["PubDate"].get('Year', '2016'))
-                mo = article["Journal"]["JournalIssue"]["PubDate"].get('Month', 'Jan')
-                try: 
-                    month = months[mo]
-                except KeyError as e:
-                    # print(e)
-                    month = int(mo[1] if mo.startswith('0') else mo)
-                day = int(article["Journal"]["JournalIssue"]["PubDate"].get('Day', '1'))
-                data = date(year,month,day)
-                query = f"insert into {self.articles_table} VALUES(%s, %s, %s, %s, %s) \
-                on conflict (pmid) do update set title=%s, abstract=%s, journal=%s, pubdate=%s;"
-                connection.execute(query, (int(pmid),title, abstract, journal, data.isoformat(), title, abstract, journal, data.isoformat()))
+            self._insert_article(pmid, art)
             new_ids[pmid] = pmid
         return new_ids
 
+    def _insert_article(self, pmid, art):
+        '''Insert article into the database'''
+        with engine.connect() as connection:
+            article = art["MedlineCitation"]["Article"]
+            try:
+                abstract = article['Abstract']['AbstractText'][0].replace(
+                    "'", "''")
+            except (AttributeError, KeyError) as e:
+                print(e)
+                abstract = ""
+            title = article["ArticleTitle"].replace("'", "''")
+            journal = article["Journal"]["Title"]
+            year = int(article["Journal"]["JournalIssue"]
+                       ["PubDate"].get('Year', '2016'))
+            mo = article["Journal"]["JournalIssue"]["PubDate"].get(
+                'Month', 'Jan')
+            try:
+                month = months[mo]
+            except KeyError as e:
+                # print(e)
+                month = int(mo[1] if mo.startswith('0') else mo)
+            day = int(article["Journal"]["JournalIssue"]
+                      ["PubDate"].get('Day', '1'))
+            data = date(year, month, day)
+            query = f"insert into {self.articles_table} VALUES(%s, %s, %s, %s, %s) \
+                on conflict (pmid) do update set title=%s, abstract=%s, journal=%s, pubdate=%s;"
+            connection.execute(query, (int(pmid), title, abstract, journal, data.isoformat(
+            ), title, abstract, journal, data.isoformat()))
 
-
+    def update_concurrently(self):
+        '''fetch articles concurrently'''
+        old_ids = self._get_old_ids()
+        print("Fetching results for {}\r".format(self.search_term))
+        handle = Entrez.esearch(
+            db="pubmed", retmax=200000, term=self.search_term)
+        response = Entrez.read(handle=handle)
+        ids = [pmid for pmid  in response['IdList'] if pmid not in old_ids]
+        print(f"Found {len(ids)} new items, out of {len(response['IdList'])}.\r")
+        new_ids = {}
+        with tqdm(total=len(ids)) as pbar:
+            with ThreadPoolExecutor(max_workers=100) as executor:
+                future_articles = {executor.submit(self._fetch, pmid): pmid for pmid in ids}
+                for future in as_completed(future_articles):
+                    pmid = future_articles[future]
+                    try:
+                        art = future.result()
+                        pbar.update(1)
+                        if art is None:
+                            continue
+                        elif "MedlineCitation" not in art:
+                            continue
+                        else:
+                            print("Saving record for {}\r".format(pmid))
+                            self._insert_article(pmid, art)
+                    except Exception as e:
+                        print(f'{pmid} generated an exception: {e}\r')
 
 
 if __name__ == "__main__":
-    S = SearchAndCapture('fccoelho@gmail.com', '((zika microcephaly) NOT zika[author])')
-    S.update_multiple_searches(zika_query_strings)
-    S.update_citations_concurrently()
-    S.create_FT_index()
-    # for s in [MERS_query_strings, Mayaro_query_strings, Oropouche_query_strings]:
-    #     T = SearchAndCapture('fccoelho@gmail.com', s[0], s[0].lower())
-    #     T.update_multiple_searches(s)
-    #     T.update_citations_concurrently()
+    # S = SearchAndCapture('fccoelho@gmail.com',
+    #                      '((zika microcephaly) NOT zika[author])')
+    # S.update_multiple_searches(zika_query_strings)
+    # S.update_citations_concurrently()
+    # S.create_FT_index()
+    for s in [Covid_query_strings,MERS_query_strings, Mayaro_query_strings, Oropouche_query_strings]:
+        T = SearchAndCapture(email='fccoelho@gmail.com', search_term=s[0], collection=s[0].lower())
+        T.update_multiple_searches(s, parallel=True)
+        T.update_citations_concurrently()
+        T.create_FT_index()
---
+++ network_analysis_psql.py
@@ -0,0 +1,113 @@
+
+import networkx as nx
+import pymongo
+from gensim import corpora, models, similarities
+from gensim.models.ldamulticore import LdaMulticore
+from nltk.tokenize import WordPunctTokenizer
+from nltk.corpus import stopwords
+from string import punctuation
+
+connection = pymongo.MongoClient()
+tokenizer = WordPunctTokenizer()
+sw = stopwords.words('english') + list(punctuation)
+
+def dump_to_csv(collection=""):
+    artcol = "articles" if not collection else collection
+    citcol = "citations" if not collection else "citations_"+collection
+    cursor_cit = connection.pubmed[citcol].find()
+    cursor_art = connection.pubmed[artcol].find()
+    f = open("{}edge_list.csv".format(collection), 'w')
+    g = open("{}node_list.csv".format(collection),'w')
+    g.write("PMID|title|journal|year|month|day\n")
+    try:
+        for cit in cursor_cit:
+            # if cit['citedby']:
+            for id_citer in cit['citedby']:
+                f.write("{},{}\n".format(id_citer, cit['PMID']))
+        for art in cursor_art:
+            article = art["MedlineCitation"]["Article"]
+            g.write("{}|{}|{}|{}|{}|{}\n".format(art["MedlineCitation"]["PMID"],
+                                                 article["ArticleTitle"],
+                                                 article["Journal"]["Title"],
+                                                 article["Journal"]["JournalIssue"]["PubDate"].get('Year', '2016'),
+                                                 article["Journal"]["JournalIssue"]["PubDate"].get('Month', 'NA'),
+                                                 article["Journal"]["JournalIssue"]["PubDate"].get('Day', 'NA'),
+                                                 ))
+    finally:
+        f.close()
+        g.close()
+
+def article_generator(artcol, filter={}, tokenize=True):
+    cursor_art = connection.pubmed[artcol].find(filter)
+    for art in cursor_art:
+        try:
+            abs = art["MedlineCitation"]["Article"]['Abstract']['AbstractText']
+        except KeyError:
+            abs = ''
+        if tokenize:
+            yield [token for token in tokenizer.tokenize(str(abs)) if token not in sw]
+        else:
+            yield abs
+
+def create_article_dictionary(collection='zika', filter=None):
+    if filter is None:
+        filter = {}
+    artcol = "articles" if collection == 'zika' else collection
+    articles = article_generator(artcol, filter=filter)
+    dictionary = corpora.Dictionary(articles)
+    dictionary.save('Dicionario_{}.dict'.format(collection))
+    return dictionary
+
+def create_corpus(collection='zika', filter=None):
+    if filter is None:
+        filter = {}
+    artcol = "articles" if collection == 'zika' else collection
+    dictionary = create_article_dictionary(collection, filter=filter)
+    stopw_ids = map(dictionary.token2id.get, sw)
+    dictionary.filter_tokens(stopw_ids)
+    dictionary.compactify()
+    dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=None)
+    dictionary.compactify()
+    # print(dictionary)
+
+    articles = article_generator(artcol, filter=filter)
+    corpus = [dictionary.doc2bow(text) for text in articles]
+    corpora.MmCorpus.serialize('corpus_{}'.format(collection), corpus)
+    return corpus, dictionary
+
+def get_top_topics_by_year(lda, full_dict, collection, year):
+    year = str(year)
+    articles = article_generator(collection, filter={"MedlineCitation.DateCreated.Year": year})
+    corpus = [full_dict.doc2bow(text) for text in articles]
+    corpus_lda = lda[corpus]
+    return sorted([doc for doc in corpus_lda], key=lambda item: -item[1])
+
+def LSI_topics(corpus, dictionary):
+    tfidf = models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+    #print(corpus_tfidf)
+    lsi = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=10)
+    return lsi
+
+def LDA_topics(corpus, dictionary, num_topics):
+    lda_model = LdaMulticore(corpus, id2word=dictionary, num_topics=num_topics, passes=10)
+    return lda_model
+
+
+if __name__ == "__main__":
+    dump_to_csv()
+    dump_to_csv('mers')
+    for col in ['zika', 'mers']:
+        print("Calculating {} lsi model".format(col))
+        c, d = create_corpus(col)
+        lsi = LSI_topics(c, d)
+        print("Saving {} lsi model".format(col))
+        lsi.save('lda_model_{}'.format(col))
+        print("Calculating {} lda model".format(col))
+        lda = LDA_topics(c, d, 30)
+        print("Saving {} lda model".format(col))
+        lda.save('lda_model_{}'.format(col))
+        doc_topics = get_top_topics_by_year(lda, d, 'zika', 2016)
+    # print(lsi.show_topics(10))
+
+
